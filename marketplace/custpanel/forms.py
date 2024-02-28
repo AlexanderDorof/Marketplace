28b35532a7d30d06f5a59c2373e1c0dc4cb8d06a
KirraDark <blackbloodyring@gmail.com>
2024-02-28 20:02:38 +0300

    Этап 3

--- marketplace/custpanel/forms.py
+++ marketplace/custpanel/forms.py
@@ -11,3 +11,13 @@ class MotocycleForm(forms.ModelForm):
     class Meta:
         model = Motocycle
         fields = '__all__'
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = '__all__'
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
