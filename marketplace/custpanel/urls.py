33c8c69d7b407d80452d2d03a8b504e648f3b8fb
Alexander <venussd96@gmail.com>
2024-03-08 00:01:34 +0300

    custom admin panel: new lists, new css

--- marketplace/custpanel/urls.py
+++ marketplace/custpanel/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
+
 from .views import *
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 app_name = 'admin-panel'
 
 urlpatterns = [
 
-    path('', models, name='models'),
+    path('', admin_home, name='admin_home'),
     path('create/', create, name='create'),
     path('delete/', delete, name='delete'),
     path('change/', change, name='change'),
-    path('list/', list, name='list'),
+    path('list/', CarsList.as_view(), name='list'),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
