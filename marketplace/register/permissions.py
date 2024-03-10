e2cfc3602cfd2b28a5ec629504458870526a5b8c
Alexander <venussd96@gmail.com>
2024-03-10 22:57:33 +0300

    profile: added feature to change password in your profile

--- marketplace/register/permissions.py
+++ marketplace/register/permissions.py
@@ -22,3 +22,24 @@ class ProfilePermissionsMixin:
                 msg_for_admin = ''
             raise Http404(f'Вы не можете заходить в профиль других пользователей.\n{msg_for_admin}')
         return super().dispatch(request, *args, **kwargs)
+
+
+class PasswordPermissionsMixin:
+    """provides functions to check if user is authorized to modify a record"""
+
+    def has_permissions(self) -> bool:
+        """checks if user is authenitcated and has permission to modify (moder/admin or creator)"""
+        if not self.request.user.is_authenticated:
+            return False
+        return self.kwargs['pk'] == self.request.user.pk
+
+    def dispatch(self, request, *args, **kwargs):
+        """if user doesn't have permission raises error, otherwise - calls dispatch function from generic class"""
+        if not self.has_permissions():
+            group = self.request.user.groups.values('name')[0]['name']
+            if group == 'admin':
+                msg_for_admin = 'Зайдите в панель администратора для редактирования записей пользователей'
+            else:
+                msg_for_admin = ''
+            raise Http404(f'Вы не можете заходить в профиль других пользователей.\n{msg_for_admin}')
+        return super().dispatch(request, *args, **kwargs)
--- marketplace/register/views.py
+++ marketplace/register/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView, UpdateView
 
 from main_app.models import User as CustomUser
-from .forms import UserRegistrationForm, ProfileForm
+from .forms import UserRegistrationForm, ProfileForm, ProfilePasswordForm
 from main_app.utils import DataMixin
-from .permissions import ProfilePermissionsMixin
+from .permissions import ProfilePermissionsMixin, PasswordPermissionsMixin
 
 
 class RegisterView(FormView):
@@ -34,6 +35,20 @@ class UserEditProfileView(ProfilePermissionsMixin, DataMixin, UpdateView):
     model = CustomUser
     extra_context = {'title': 'Редактирование профиля'}
     form_class = ProfileForm
+    template_name = 'register/profile.html'
+    success_url = reverse_lazy('home')
+    login_url = 'register:login'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context()
+        context = {**context, **c_def}
+        return context
+
+class UserEditPasswordView(PasswordPermissionsMixin, DataMixin, UpdateView):
+    model = User
+    extra_context = {'title': 'Редактирование профиля'}
+    form_class = ProfilePasswordForm
     template_name = 'register/profile.html'
     success_url = reverse_lazy('home')
     login_url = 'register:login'
