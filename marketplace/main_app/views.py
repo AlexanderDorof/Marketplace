a946c61a572c1940bd2a4c4e09d98b5f3795bba1
Alexander <venussd96@gmail.com>
2024-03-11 03:49:16 +0300

    added resizing images added it to signals

--- marketplace/main_app/views.py
+++ marketplace/main_app/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 from .models import *
 from .utils import *
 from .tasks import send_email_task
+
 
 
 
